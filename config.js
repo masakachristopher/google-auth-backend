dadbb2df7c0940706b88871a7ce879455d8ab95c
masakachristopher <masakachristopher@yahoo.com>
2022-02-20 17:37:38 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export const google = {
+    clientID: '835183820059-n1verkusfer4digh75n71am7965v64pe.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+};
